41719917a1a70accd2df88d2f6e028c1adf58717
Rémy Perona <remperona@gmail.com>
2025-05-07 11:58:35 -0400

    add unit tests base setup

---
+++ tests/Unit/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace WPMedia\Mixpanel\Tests\Unit;
+
+define( 'WPM_MIXPANEL_PLUGIN_ROOT', dirname( dirname( __DIR__ ) ) . DIRECTORY_SEPARATOR );
+define( 'WPM_MIXPANEL_TESTS_FIXTURES_DIR', dirname( __DIR__ ) . '/Fixtures' );
+define( 'WPM_MIXPANEL_TESTS_DIR', __DIR__ );
+define( 'WPM_MIXPANEL_IS_TESTING', true );

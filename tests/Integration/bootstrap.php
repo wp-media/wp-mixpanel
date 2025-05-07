430957403cfd5eb13e1e9d6b8ac8e535af603b4c
Rémy Perona <remy@wp-media.me>
2025-05-07 12:08:54 -0400

    Add PHPUnit unit & integration tests setup (#4)

---
+++ tests/Integration/bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+
+namespace WPMedia\Mixpanel\Tests\Integration;
+
+define( 'WPM_MIXPANEL_PLUGIN_ROOT', dirname( dirname( __DIR__ ) ) . DIRECTORY_SEPARATOR );
+define( 'WPM_MIXPANEL_TESTS_FIXTURES_DIR', dirname( __DIR__ ) . '/Fixtures' );
+define( 'WPM_MIXPANEL_TESTS_DIR', __DIR__ );
+define( 'WPM_MIXPANEL_IS_TESTING', true );
